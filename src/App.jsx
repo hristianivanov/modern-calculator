378c07000d4e2c2cb815f02811979469d72c0aea
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2024-08-17 21:12:23 +0300

    configure dark mode styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import './App.css'
+import './index.css'
+
+import Header from './components/header/Header'
+import Keyboard from './components/keyboard/Keyboard'
+import Screen from './components/screen/Screen'
+
+import { ThemeContextProvider } from './context/ThemeContext'
+import ThemeProvider from './providers/ThemeProvider'
 
 
 function App() {
 
   return (
-    <>
-    </>
+    <ThemeContextProvider>
+      <ThemeProvider>
+
+        <div className='container'>
+          <div className='calculator'>
+
+            <Header />
+            <div>
+              <Screen />
+              <Keyboard />
+            </div>
+
+          </div>
+        </div>
+      </ThemeProvider>
+    </ThemeContextProvider>
   )
 }
 
--- src/components/keyboard/Keyboard.jsx
+++ src/components/keyboard/Keyboard.jsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
 import styles from './keyboard.module.css';
+import { ThemeContext } from '../../context/ThemeContext';
 
 export default function Keyboard() {
+
+    const { theme } = useContext(ThemeContext)
 
     const handleOperator = (e) => {
 
@@ -48,6 +52,26 @@ export default function Keyboard() {
                     </div>
                 </div>
                 <div className={styles.background}></div>
+                {theme === "dark" &&
+                    <svg className={styles.svg} width="423" height="507" viewBox="0 0 423 507" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g filter="url(#filter0_f_2_78)">
+                            <path d="M697.404 -24.1603C819.447 325.921 820.324 583.616 315.636 503.575C-7.67762 350.43 -155.022 587.082 -481.876 488.638C-246.465 -103.199 -160.092 354.76 45.0564 266.06C300.809 155.48 136.128 -793.125 697.404 -24.1603Z" fill="url(#paint0_linear_2_78)" />
+                        </g>
+                        <defs>
+                            <filter id="filter0_f_2_78" x="-845.876" y="-686.261" width="1974.7" height="1568.5" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+                                <feGaussianBlur stdDeviation="182" result="effect1_foregroundBlur_2_78" />
+                            </filter>
+                            <linearGradient id="paint0_linear_2_78" x1="641.33" y1="-233.083" x2="-123.056" y2="690.561" gradientUnits="userSpaceOnUse">
+                                <stop offset="0.0238694" stop-color="#42869B" />
+                                <stop offset="0.277847" stop-color="#2A7DA1" />
+                                <stop offset="0.650876" stop-color="#224E91" />
+                                <stop offset="1" stop-color="#00123F" />
+                            </linearGradient>
+                        </defs>
+                    </svg>
+                }
             </div>
         </>
     );
