ef0428b85a041c54e30744e0cfd66cd0558e0ff3
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2024-08-19 06:09:57 +0300

    Separate logic to app.jsx

--- src/components/keyboard/Keyboard.jsx
+++ src/components/keyboard/Keyboard.jsx
@@ -2,151 +2,15 @@ import { useContext, useState } from 'react';
 import styles from './keyboard.module.css';
 import { ThemeContext } from '../../context/ThemeContext';
 
-export default function Keyboard() {
+export default function Keyboard({
+    handleOperand,
+    handleOperator
+}) {
 
     const { theme } = useContext(ThemeContext)
 
-    const [prevAnswer, setPrevAnswer] = useState("");
-    const [answer, setAnswer] = useState("0");
-    const [operand, setOperand] = useState("");
-
-    const handleOperator = (e) => {
-        const value = e.target.value;
-
-
-        if (value === "=") {
-            if (operand === "") return;
-        }
-
-        // if no value in operand stop 
-        if (value === "ac") {
-            setOperand("");
-            setAnswer(0);
-
-            // Check if we have a prev answer > 0
-            if (answer > 0)
-                setPrevAnswer(answer);
-            return;
-        }
-
-        // handle plush and minus sign
-        if (value === "pm") {
-            if (operand === "") return;
-            //get the last char
-            let calculated;
-            if (Number(operand.slice(-1))) {
-                calculated = eval(operand);
-
-                if (Math.sign(calculated) < 0) {
-                    calculated = Math.abs(calculated);
-                    setOperand(calculated.toString());
-                } else {
-                    setOperand(`-` + calculated.toString());
-                }
-
-            } else {
-                calculated = (eval(operand.slice(0, -1)));
-                if (Math.sign(calculated)) {
-                    setOperand((`-` + calculated.toString()))
-                } else {
-                    setOperand((calculated.toString()))
-                }
-            }
-            return;
-        }
-
-        /* last test for users */
-        if (value === "%") {
-            if (operand === "") return;
-        }
-
-        let newOperand;
-        // get last operand value
-        if (operand.slice(-1) === value) {
-            newOperand = operand.slice(0, -1);
-            setOperand(newOperand + value);
-        } else {
-            // get the last input operator & check if is a number
-            if (!Number(operand.slice(-1))) {
-                // remove the last selected char
-                newOperand = operand.slice(0, -1);
-
-                // checks if the last operand contains a zero
-                if (Number(operand.slice(-1)) === 0) {
-                    setOperand(newOperand + `0` + value); return
-                } else {
-                    setOperand(newOperand + value);
-                    return;
-                }
-
-            } else if (operand.slice(-1) === "ac") {
-
-                setOperand("");
-                // Check if we have a prev answer > 0
-                if (answer > 0)
-                    setAnswer(0)
-            }
-            else if (operand.includes("/")) {
-                newOperand = eval(operand);
-                setOperand(newOperand);
-            }
-        }
-
-
-        // if the last inputed digit is not a number stop
-        const lastDigit = operand.slice(-1);
-        if (!Number(lastDigit)) return;
-
-        // if Dot(.) exists don't add again
-        if (!(operand === "." || operand.includes("."))) {
-            setOperand(operand => operand + value);
-        }
-
-        // Swicth for some arithmetic operations
-        switch (value) {
-            case "ac":
-                setOperand("");
-                break;
-            case "+":
-                setOperand(eval(operand) + value)
-                break;
-            case "-":
-                setOperand(`${eval(operand)}${value}`)
-                break;
-            case "*":
-                setOperand(`${eval(operand)}${value}`)
-                break;
-            case "%":
-                console.log('percentage + Test for all viewers')
-                break;
-            case "/":
-                setOperand(`${eval(operand)}${value}`);
-                break;
-            case "=":
-                setOperand("");
-                setAnswer(eval(operand));
-                if (answer > 0)
-                    setPrevAnswer(answer);
-                break;
-            default:
-                return;
-        }
-
-    }
-
-    const handleOperand = (e) => {
-        const value = e.target.value;
-
-        setOperand(operand => operand + value);
-    }
-
     return (
         <>
-            <div className={`${styles.containerS} wrapper`}>
-                <div className={styles.history}>{operand ? operand : ''} </div>
-                <div className={styles.sign}>=</div>
-                <div className={styles.result}>{answer}</div>
-            </div>
             <div className={styles.container}>
                 <div className='wrapper'>
                     <div className={styles.calculator}>
--- src/components/screen/Screen.jsx
+++ src/components/screen/Screen.jsx
@@ -1,11 +1,14 @@
 import styles from './screen.module.css'
 
-export default function Screen() {
+export default function Screen({
+  answer,
+  operand
+}) {
   return (
     <div className={`${styles.container} wrapper`}>
       <div className={styles.history}>{operand ? operand : '0'} </div>
       <div className={styles.sign}>=</div>
-      <div className={styles.result}>2004</div>
+      <div className={styles.result}>{answer}</div>
     </div>
   );
 }
